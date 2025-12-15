f25df1cbaecf0cf2c7e1bd9965a502e6dff6f910
Wei Deng <dw-dengwei@outlook.com>
2025-12-15 11:17:34 +0800

    Update blog configuration with new author details

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'Wei\'s Site',
+  author: 'Wei Deng',
+  email: 'dw-dengwei@outlook.com',
+  link: 'https://dw-dengwei.cn',
+  description: 'Wei Deng\'s personal website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
